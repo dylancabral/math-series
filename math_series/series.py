299906a37effe607e88751d5559772810ba05072
Dylan Cabral <dylancabral.93@gmail.com>
2023-01-15 15:14:49 -0800

    updating tests and read me and uncommenting sum seriez

--- math_series/series.py
+++ math_series/series.py
@@ -1,32 +1,40 @@
 import pytest
 
-def test_one():
-    return "goodbye"
+# def test_one():
+#     return "goodbye"
+
+
 def fibonacci(n):
+    """
+    Finds the n-th index of the fibonacci sequence
+    """
     if n in{0,1}:
         return n
     return fibonacci(n -1) + fibonacci(n-2)
 
 def lucas(n):
+    """
+    Finds the n-th index of the lucas sequence
+    """
     if n == 0:
         return 2
     if n == 1:
         return 1
     return lucas(n-1) + lucas(n-2)
 
-# def sum_series(n, x=0, y=1):
-#     """
-#     Finds the n-th index of a new sequence that takes the sum of the previous two elements.
-#     """
-#     if isinstance(n, int) and isinstance(x, int) and isinstance(y, int):
-#         if n == 0:
-#             return x
-#         if n == 1:
-#             return y
-#         if n > 1:
-#             return sum_series(n - 1, x, y) + sum_series(n - 2, x, y)
-#         else:
-#             return "Please enter a valid input"
-#     else:
-#         return "Please enter a valid input"
+def sum_series(n, x=0, y=1):
+    """
+    Finds the n-th index of a new sequence that takes the sum of the previous two elements.
+    """
+    if isinstance(n, int) and isinstance(x, int) and isinstance(y, int):
+        if n == 0:
+            return x
+        if n == 1:
+            return y
+        if n > 1:
+            return sum_series(n - 1, x, y) + sum_series(n - 2, x, y)
+        else:
+            return "Please enter a valid input"
+    else:
+        return "Please enter a valid input"
     
